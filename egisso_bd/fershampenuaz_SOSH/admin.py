baa69be2ef33fef3ba99b22018d40d062b9197af
Ubuntu <osoben1@yandex.ru>
2022-03-21 16:35:59 +0000

    save 21.03.2022

---
+++ egisso_bd/fershampenuaz_SOSH/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import fershampenuaz_SOSH_LP
+
+admin.site.register(fershampenuaz_SOSH_LP)
---
+++ egisso_bd/fershampenuaz_SOSH/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class FershampenuazSoshConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'fershampenuaz_SOSH'
+    verbose_name = 'Фершампенуазская СОШ льготное питание'
